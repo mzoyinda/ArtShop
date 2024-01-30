bd072c35c8aa5bd0e6bfa44a00325632f510f614
oyindamola <oyindamoladawodu22@gmail.com>
2024-01-30 08:18:21 +0100

    basic next setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import App from "./components/App";
+import App from "../components/App";
 
 const inter = Inter({ subsets: ["latin"] });
 
